c7b5e17dc08abe789f28b66579e4af5d9ee1abf3
M Sparsh Mehra <114851205+M-Sparsh-Mehra@users.noreply.github.com>
2025-07-21 21:57:46 +0000

    esn training pipeline completed

--- src/train_esn.py
+++ src/train_esn.py
@@ -1,17 +1,21 @@
+""" Trains an ESN model for a given pollutant """
+
+
 import os
 import numpy as np
 import joblib
-from pyESN import ESN
+from src.pyESN import ESN
 from itertools import product
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import root_mean_squared_error, r2_score
 from src.preprocessing import load_raw_data
-from src.config import DATA_RAW_PATH, MODEL_DIR,POLLUTANTS
+from src.config import DATA_RAW_PATH, MODEL_DIR,POLLUTANTS,FORECAST_HORIZON
 import mlflow
 import mlflow.sklearn
 
 # trains on specefic feature in the pollutants list
-def train_esn_pollutant(df, feature, forecast_horizon=6, n_reservoir=200, sparsity=0.2, spectral_radius=0.95,activation="tanh"):
+# this happens for one feature at a time
+def train_esn_pollutant(df, feature, forecast_horizon=FORECAST_HORIZON, n_reservoir=200, sparsity=0.2, spectral_radius=0.95,activation="tanh",return_model=False):
         series = df[feature].values   #time series of that specefic feature
         n = len(series) - forecast_horizon
         X = series[:n].reshape(-1, 1) # X will be my first n series values
@@ -27,6 +31,12 @@ def train_esn_pollutant(df, feature, forecast_horizon=6, n_reservoir=200, sparsi
         Y_train, Y_val = Y_scaled[:trainlen], Y_scaled[trainlen:]
 
         # mlflows 
+        # if an active run exists, end it
+
+        #set mlflows directry
+        mlflow.set_tracking_uri("/workspaces/urban-air-quality-index-predictor/mlruns")
+        if mlflow.active_run():
+               mlflow.end_run()
         with mlflow.start_run(run_name=f"ESN_{feature}"):
                 mlflow.set_tag("pollutant", feature)
                 mlflow.log_params({
@@ -34,26 +44,22 @@ def train_esn_pollutant(df, feature, forecast_horizon=6, n_reservoir=200, sparsi
                 "sparsity": sparsity,
                 "spectral_radius": spectral_radius})
 
-        params = { 
-                "n_reservoir":200 ,
-                "sparsity":0.2 ,
-                "spectral_radius": 0.95,
-                "leak_rate": 1.0,
-                "activation":"tanh"}
-
         esn=ESN(
             n_inputs=1,
             n_outputs=1,
             n_reservoir=n_reservoir,
             sparsity=sparsity,
             spectral_radius=spectral_radius,
-            activation=activation,
             random_state=0)
         
+        #fitting ESN
         esn.fit(X_train, Y_train)
-        Y_pred = esn.predict(X_val)
         
-        rmse = root_mean_squared_error(Y_val, Y_pred, squared=False)
+        # predicting y
+        Y_pred = esn.predict(X_val)
+
+        # error metrics
+        rmse = root_mean_squared_error(Y_val, Y_pred)
         r2 = r2_score(Y_val, Y_pred)
 
 
@@ -67,7 +73,12 @@ def train_esn_pollutant(df, feature, forecast_horizon=6, n_reservoir=200, sparsi
             "scaler_y": scaler_y,
             "forecast_horizon": forecast_horizon
         }
+
+        
+
         save_path = os.path.join(MODEL_DIR, f"esn_{feature.lower().replace('.', '')}.pkl")
         joblib.dump(model_dict, save_path)
         mlflow.log_artifact(save_path)
         
+        if return_model:
+            return esn, rmse
