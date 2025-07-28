8ae812fec5fcd1ba3e641a2fce568d6793061fd1
M-Sparsh-Mehra <sparshmehra98999@gmail.com>
2025-07-28 16:48:00 +0530

    Stop tracking models folder

--- src/config.py
+++ src/config.py
@@ -1,10 +1,10 @@
 import os
 
 # general paths
-DATA_RAW_PATH = "/workspaces/urban-air-quality-index-predictor/data/raw/"
-DATA_PROCESSED_PATH = "/workspaces/urban-air-quality-index-predictor/data/processed/processed_data.csv"
-PREDICTIONS_PATH = "/workspaces/urban-air-quality-index-predictor/data/predictions/"
-MODEL_DIR = "/workspaces/urban-air-quality-index-predictor/models/"
+DATA_RAW_PATH = r"D:\SPARSH\pollution data\urban-air-quality-index-predictor\data\raw"
+DATA_PROCESSED_PATH = r"D:\SPARSH\pollution data\urban-air-quality-index-predictor\data\processed\processed_data.csv"
+PREDICTIONS_PATH = r"D:\SPARSH\pollution data\urban-air-quality-index-predictor\data\predictions"
+MODEL_DIR = r"D:\SPARSH\pollution data\urban-air-quality-index-predictor\models"
 
 features=["StationId", "Date", "PM2.5", "PM10", "NO", "NO2", "NOx", "NH3", "CO", "SO2", "O3", "Benzene", "Toluene", "Xylene", "AQI", "AQI_Bucket"]
 
